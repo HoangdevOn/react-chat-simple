0516d7b6265ac2ec3fee5551202f4f5b8a077b17
Vũ Đình Hoàng <90261802+HoangdevOn@users.noreply.github.com>
2023-03-04 04:07:29 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,17 +5,16 @@ import { getAnalytics } from "firebase/analytics";
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  apiKey: "AIzaSyDE0XhMhhMp-BLHc-C8mhU1xsxKV5E6-NE",
+  authDomain: "chatappsimple-e9598.firebaseapp.com",
+  projectId: "chatappsimple-e9598",
+  storageBucket: "chatappsimple-e9598.appspot.com",
+  messagingSenderId: "967578319081",
+  appId: "1:967578319081:web:29ac29ce21b85be99f1878",
+  measurementId: "G-THEDHBYCPR"
 };
 
 // Initialize Firebase
